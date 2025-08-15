c199e97e809cf25ebdab1c39cf5686ad5853db25
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-08-15 15:44:34 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_15_063826) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_15_064318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
